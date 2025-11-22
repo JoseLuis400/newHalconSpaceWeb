78372b8f9de336bc488bb1823cbb7aecfb965331
JoseLuis400 <153861602+LanzamientosEspaciales@users.noreply.github.com>
2025-11-21 23:34:35 -0300

    Update articulo-mision.js

--- js/articulo-mision.js
+++ js/articulo-mision.js
@@ -227,7 +227,11 @@ if(lanzamiento.window && lanzamiento.window.start && lanzamiento.window.end && l
 
   const elemento = document.getElementById("propulsorAUsar");
     if (elemento) {
+      if (lanzamiento.propulsor) {
         elemento.textContent = lanzamiento.propulsor;
+      } else {
+        elemento.textContent = "Por confirmar";
+      }
     }
 
   if (contadorInterval) clearInterval(contadorInterval);
